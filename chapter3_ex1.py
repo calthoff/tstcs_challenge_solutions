3c9f6dcb38a5cd88f93f02c2a0b2b01bdb481eee
Cory Edward Althoff <coryalthoff1@Corys-MacBook-Pro.local>
2021-10-14 22:27:09 -0700

    fix code

--- chapter3_ex1.py
+++ chapter3_ex1.py
@@ -1,19 +1,19 @@
-# not done
-
 def binary_search(a_list, c):
     first = 0
     last = len(a_list) - 1
-    char_code = ord(c)
+    target_code = ord(c)
     while last >= first:
         mid = (first + last)//2
         if a_list[mid] == c:
             return True
         else:
-            if char_code < ord(a_list[mid]):
-                last = mid - 1
-            else:
+            if target_code > ord(a_list[mid]):
                 first = mid + 1
+            else:
+                last = mid - 1
 
     return False
 
-print(binary_search(['s','e','l','f'], 'f'))
+print(binary_search(['a', 'b', 'c', 'd'], 'b'))
+
+
