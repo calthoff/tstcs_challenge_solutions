6ddf23e3a17b1938600af32e8ac92e53096993d9
Cory Edward Althoff <coryalthoff1@Corys-MacBook-Pro.local>
2021-10-16 11:20:08 -0700

    fix queue problem

--- chap12_ex1.py
+++ chap12_ex1.py
@@ -1,5 +1,3 @@
-# not finished yet
-
 class Queue:
     def __init__(self):
         self.s1 = []
@@ -11,11 +9,10 @@ class Queue:
     def dequeue(self):
         if len(self.s1) == 0:
             raise Exception("Cannot pop from empty queue")
-        while len(self.s1) != 1:
-            item = self.s1.pop()
-            self.s2.append(item)
-        self.s1.pop()
-        while len(self.s2) > 0:
+        while len(self.s1) != 0:
+            self.s2.append(self.s1.pop())
+        item = self.s2.pop()
+        while len(self.s2) >= 1:
             self.s1.append(self.s2.pop())
         return item
 
@@ -24,5 +21,7 @@ queue = Queue()
 queue.enqueue(1)
 queue.enqueue(2)
 queue.enqueue(3)
-for i in range(3):
+queue.enqueue(4)
+queue.enqueue(5)
+for i in range(5):
     print(queue.dequeue())
